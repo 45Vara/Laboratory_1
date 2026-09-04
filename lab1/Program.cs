da792f30445fea2bf2b953d4bfd40ccd6b09e6ac
Варвара_Голубева <varvragoludeva07@mail.ru>
2026-09-04 16:03:35 +0700

    initial

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -8,8 +8,19 @@ namespace lab1
 {
     internal class Program
     {
+        public List<StudentTopic> L = new List<StudentTopic>();
         static void Main(string[] args)
         {
+        }
+        public void SortbyName(string name, List<StudentTopic> L)
+        {
+            foreach (var i in L)
+            {
+                if (i.NameStudent == name)
+                {
+                    Console.WriteLine(i.StusentsTopic);
+                }
+            }
         }
     }
 }
---
+++ lab1/StudentTopic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class StudentTopic
+    {
+        public string NameStudent { get; set; }
+        public string StusentsTopic { get; set; }
+        public DateTime Date {  get; set; }
+
+        public StudentTopic(string nameStudent, string stusentTopic, DateTime date)
+        {
+            NameStudent = nameStudent;
+            StusentTopic = stusentTopic;
+            Date = date;
+        }
+    }
+}
